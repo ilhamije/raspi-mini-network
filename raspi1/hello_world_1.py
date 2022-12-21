1ab5c12e83e3381df9d00623370a81142c19fd45
Ilham Akbar <ilhamije@gmail.com>
2022-12-21 10:26:47 +0700

    add both backend endpoint with image load

--- raspi1/hello_world_1.py
+++ raspi1/hello_world_1.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 
 app = Flask(__name__)
@@ -6,5 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Raspi 1 said : Hello Jarkomjut'
+
+@app.route('/image_test')
+def image_test():
+    filename = 'makara-iluni-hitam.png'
+    return send_from_directory("", filename)
+
+
 if __name__ == '__main__':
     app.run(port=5001, host='0.0.0.0')
