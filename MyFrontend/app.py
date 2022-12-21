085ac8c12659197464b98021aca1e3bfa389c3c7
Ilham Akbar <ilhamije@gmail.com>
2022-12-21 10:37:05 +0700

    update frontend and siege command

--- MyFrontend/app.py
+++ MyFrontend/app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    url = 'http://192.168.1.13'
-    response = requests.get(url)
+    LB_URL = 'http://192.168.1.17'
+    BACKEND1_URL = 'http://192.168.1.12'
+
+    response = requests.get(LB_URL)  # change url to use between LB and BE
     result = response.text
 
     return render_template("welcome.html",hello_data=result)
--- siege-test.sh
+++ siege-test.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
-for i in 1 10 100
+
+FRONTEND_IPADDR=""
+
+
+for i in 2 10 100
 do
+  echo "direct hit from frontend to backend"
+  siege $FRONTEND_IPADDR:5003 -c -$i -r 300 -t 2m -l > logs/fe-concurrent-$i-directbackend.json
+
   echo "Process .. c = $i"
   # echo "lbmethod=bytraffic"
-  # siege 192.168.1.11:5001 -c $i -t 2m -l > logs/be-concurrent-$i.json
-  # siege 192.168.1.13 -c $i -t 2m -l > logs/lb-concurrent-$i.json
-  # siege 192.168.1.13:5003 -c $i -t 2m -l > logs/fe-concurrent-$i.json
+  # siege 192.168.1.11:5001 -c $i -t 2m -l > logs/be-concurrent-$i-bytraffic.json
+  # siege 192.168.1.13 -c $i -t 2m -l > logs/lb-concurrent-$i-bytraffic.json
+  # siege 192.168.1.13:5003 -c $i -t 2m -l > logs/fe-concurrent-$i-bytraffic.json
 
-  # echo "lbmethod=byrequests"
+  echo "lbmethod=byrequests"
   # siege 192.168.1.17 -c $i -t 2m -l > logs/lb-concurrent-$i-byrequests.json
   # siege 192.168.1.17:5003 -c $i -t 2m -l > logs/fe-concurrent-$i-byrequests.json
   # siege 192.168.1.15:5001 -c $i -t 2m -l > logs/be-concurrent-$i-byrequests.json
 
-  # echo "lbmethod=bybusyness"
+  echo "lbmethod=bybusyness"
   # siege 192.168.1.17 -c $i -t 2m -l > logs/lb-concurrent-$i-bybusyness.json
   # siege 192.168.1.17:5003 -c $i -t 2m -l > logs/fe-concurrent-$i-bybusyness.json
   # siege 192.168.1.15:5001 -c $i -t 2m -l > logs/be-concurrent-$i-bybusyness.json
 
-
-  echo "direct hit from frontend to backend"
-  siege 192.168.1.17:5003 -c $i -t 2m -l > logs/fe-concurrent-$i-directbackend.json
 done
